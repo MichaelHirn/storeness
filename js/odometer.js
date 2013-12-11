3d60f3c4472209ac8c1580e5da5935b4ed6721d4
Michael Hirn <michael@repricing.de>
2013-12-11 17:52:30 +0100

    testing Iphone Counter Bug

--- js/odometer.js
+++ js/odometer.js
@@ -17,6 +17,6 @@ var startValue = 60169356;
 var startDate  = new Date("2013 nov 17");
 var nowDate    = new Date();
 var diff       = (nowDate - startDate)/1000;
-var add        = Math.round(diff/30);
+var add        = 0;
 
 od.update(startValue+add);
